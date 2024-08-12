099ad6a6b2bd3f551a389526333aaa29d543a6ca
karthikayan.r <karthikayan.r@ciglobalsolutions.com>
2024-08-13 01:36:50 +0530

    first commit90953372

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1971459180"
+    serviceWorkerVersion: "2874152886"
   }
 });
